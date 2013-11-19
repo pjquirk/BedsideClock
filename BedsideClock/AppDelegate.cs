b50f69dc312616c5facc43c5274163ef7fc9d648
Patrick Quirk <quirker@gmail.com>
2013-11-18 21:48:01 -0500

    Made options a local variable since it doesn't matter how long we hang on to it

--- BedsideClock/AppDelegate.cs
+++ BedsideClock/AppDelegate.cs
@@ -17,7 +17,6 @@ namespace BedsideClock
 		UIWindow window;
 		UINavigationController rootViewController;
 		Options.OptionsViewController optionsViewController;
-		Model.Options options;
 		//
 		// This method is invoked when the application has loaded and is ready to run. In this
 		// method you should instantiate the window, load the UI into it and then make the window
@@ -27,7 +26,7 @@ namespace BedsideClock
 		//
 		public override bool FinishedLaunching(UIApplication application, NSDictionary launchOptions)
 		{
-			LoadOptions();
+			Model.Options options = LoadOptions();
 
 			// create a new window instance based on the screen size
 			window = new UIWindow(UIScreen.MainScreen.Bounds);
@@ -64,10 +63,10 @@ namespace BedsideClock
 			serializer.Save(optionsViewController.GetOptions());
 		}
 
-		void LoadOptions()
+		Model.Options LoadOptions()
 		{
 			OptionsSerializer serializer = new OptionsSerializer();
-			options = serializer.Load();
+			return serializer.Load();
 		}
 	}
 }
